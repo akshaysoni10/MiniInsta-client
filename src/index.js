9192a6d4be431fab124c1467bf6f229e44f32b1a
Akshay Soni <109035961+akshaysoni10@users.noreply.github.com>
2023-05-24 13:01:21 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
-import {createStore, applyMiddleware, compose} from 'redux';
+import { configureStore } from '@reduxjs/toolkit';
 import thunk from 'redux-thunk';
 
 import reducers from './reducers';
@@ -9,7 +9,10 @@ import reducers from './reducers';
 import App from './App';
 import './index.css'; 
 
-const store = createStore(reducers, compose(applyMiddleware(thunk)));
+const store = configureStore({
+    reducer: reducers,
+    middleware: [thunk]
+});
 
 ReactDOM.render(
     <Provider store={store}>
